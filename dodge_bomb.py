489043e78d44dd5647209a03f0ea6a8d82558634
c0a25008a5 <c0a25008a5@edu.teu.ac.jp>
2026-06-16 17:12:11 +0900

    追加課題3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -28,22 +28,22 @@ def chech_bound(rct: pg.Rect)->tuple[bool,bool]:#練3外いかない
     return yoko,tate
 
 def gameover(screen: pg.Surface) -> None:
-    go_img=pg.Surface((1100,650))
+    go_img=pg.Surface((1100,650))#黒い図
     pg.draw.rect(go_img,(0,0,0),pg.Rect(0,0,1100,650))
     go_img.set_alpha(180)
 
-    fonto=pg.font.Font(None,100)
+    fonto=pg.font.Font(None,100)#文字
     txt=fonto.render("Game Over",True,(255,255,255))
     txt_rct = txt.get_rect()
     txt_rct.center = 1100 // 2, 650 // 2
     go_img.blit(txt,txt_rct)
 
-    kc_img_l = pg.image.load("fig/8.png")
+    kc_img_l = pg.image.load("fig/8.png")#泣く
     kc_img_r = pg.transform.flip(kc_img_l, True, False)
     kc_rct_l = kc_img_l.get_rect()
-    kc_rct_l.center = WIDTH // 2 - 200, HEIGHT // 2
+    kc_rct_l.center = 1100 // 2 - 200, 650 // 2
     kc_rct_r = kc_img_r.get_rect()
-    kc_rct_r.center = WIDTH // 2 + 200, HEIGHT // 2
+    kc_rct_r.center = 1100 // 2 + 200, 650 // 2
     
     go_img.blit(kc_img_l, kc_rct_l)
     go_img.blit(kc_img_r, kc_rct_r)
@@ -62,6 +62,27 @@ def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
     bb_accs = [a for a in range(1, 11)]
     return bb_imgs, bb_accs
 
+def get_kk_imgs() -> dict[tuple[int, int], pg.Surface]:
+    """
+    押下キーに対する移動量の合計値タプルをキー、
+    rotozoomしたSurfaceを値とした辞書を返す
+    """
+    base_img = pg.image.load("fig/3.png")
+    flip_img = pg.transform.flip(base_img, True, False)
+
+    kk_dict={
+        (0, 0): pg.transform.rotozoom(flip_img, 0, 0.9),      # キー押下がない場合（初期状態・右向き）
+        (+5, 0): pg.transform.rotozoom(flip_img, 0, 0.9),     # 右
+        (+5, -5): pg.transform.rotozoom(flip_img, 45, 0.9),   # 右上
+        (0, -5): pg.transform.rotozoom(flip_img, 90, 0.9),    # 上
+        (-5, -5): pg.transform.rotozoom(base_img, -45, 0.9),  # 左上
+        (-5, 0): pg.transform.rotozoom(base_img, 0, 0.9),     # 左
+        (-5, +5): pg.transform.rotozoom(base_img, 45, 0.9),   # 左下
+        (0, +5): pg.transform.rotozoom(flip_img, -90, 0.9),   # 下
+        (+5, +5): pg.transform.rotozoom(flip_img, -45, 0.9),  # 右下
+    }
+    return kk_dict
+
 
 
 
@@ -70,15 +91,18 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    kk_imgs = get_kk_imgs()
+    # kk_img = kk_imgs[(0, 0)]
+
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
 
     bb_imgs, bb_accs = init_bb_imgs()
     
     #練2　爆弾挿入
-    # bb_img = pg.Surface((20,20)) #円のサイズ
-    # pg.draw.circle(bb_img,(255,0,0),(10,10),10) #
-    # bb_img.set_colorkey((0,0,0)) # 黒い部分を透明にする
+    bb_img = pg.Surface((20,20)) #円のサイズ
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10) #
+    bb_img.set_colorkey((0,0,0)) # 黒い部分を透明にする
     bb_img = bb_imgs[0]
     bb_rct = bb_img.get_rect()
     # 画面内のランダムな位置
@@ -114,6 +138,8 @@ def main():
         kk_rct.move_ip(sum_mv)
         if chech_bound(kk_rct) !=(True,True):#練3外いかない2
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])#動きをなかったことに
+        
+        kk_img = kk_imgs[tuple(sum_mv)]
         screen.blit(kk_img, kk_rct)
 
         bb_rct.move_ip(vx,vy) #練2爆弾動く
